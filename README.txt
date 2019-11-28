4ae8f02f49ef06ae0fbc83d49ce39c83f3f22529
Lucas Sales <sales_l@hotmail.com>
2019-11-28 05:12:01 -0300

    Speeling fixes, and implemente of README

---
+++ README.txt
@@ -0,0 +1,9 @@
+This is a calculator of epic coins from the game ''Grand Chase'' 
+Each Epic mission gives 1 to 3 epic coins and has level criteria.
+Fornalha infernal acess starting from level 30 = Gives 1 epic coin
+Altar da ruína acess starting from level 40= Gives 1 epic coin
+Torre das ilusões acess starting from level 80 
+= This tower has 30 floors separeted this way:
+1  - 10 = gives 1 epic coin
+11 - 20 = gives 2 epic coins
+21 - 30 = gives 3 epic coins
--- src/applications/Main.java
+++ src/applications/Main.java
@@ -9,24 +9,24 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Ola");
-		System.out.println("quantos personagens estao aptos para ir fornalha?");
+		System.out.println("Quantos personagens estao aptos para ir Fornalha Infernal?");
 		int personagens = sc.nextInt();
 		Desafios fornalha = new Desafios(personagens);
-		System.out.println("quantos personagens estao aptos para ir altar?");
+		System.out.println("Quantos personagens estao aptos para ir Altar da Ruína?");
 		personagens = sc.nextInt();
 		Desafios altar = new Desafios(personagens);
-		System.out.println("quantos personagens estao aptos para ir torre?");
+		System.out.println("Quantos personagens estao aptos para ir Torre das Ilusões?");
 		personagens = sc.nextInt();
 		Desafios torre = new Desafios(personagens);
 
-		System.out.println("vc ja fez algum desafio hoje?Responda 'y' para sim e 'n' para nao");
+		System.out.println("Você já realizou algum desafio épico hoje?Responda 'y' para sim e 'n' para não.");
 		char bol = sc.next().charAt(0);
 		if (bol == 'n') {
 			fornalha.addMoeda(4*fornalha.getPersonagens());
 			altar.addMoeda(4*altar.getPersonagens());
 
 			for (int i = 0; i < torre.getPersonagens()*4; i++) {
-					System.out.println("quais andares pretende ir?");
+					System.out.println("Quais andares pretende ir?");
 					int andar = sc.nextInt();
 					if (andar <= 10) {
 						torre.addMoeda(1);
@@ -41,15 +41,15 @@ public class Main {
 			
 		
 		else {
-			System.out.println("quantidade de vezes jogadas fornalha:");
+			System.out.println("Quantidade de vezes jogadas Fornalha Infernal:");
 			int qtd = sc.nextInt();
 			fornalha.addMoeda((4)*(fornalha.getPersonagens())- qtd);
-			System.out.println("quantidade de vezes jogadas altar:");
+			System.out.println("Quantidade de vezes jogadas Altar da Ruína:");
 			qtd = sc.nextInt();
 			altar.addMoeda((4)*(altar.getPersonagens()) - qtd);
-			System.out.println("quantidade de vezes jogada torre:");
+			System.out.println("Quantidade de vezes jogada Torre das Ilusões:");
 			qtd = sc.nextInt();
-			System.out.println("quais andares ainda pretende ir");
+			System.out.println("Quais andares ainda pretende ir?");
 			for (int i = 0; i < (torre.getPersonagens()*4) - qtd; i++) {
 				int andar = sc.nextInt();
 				if (andar <= 10) {
